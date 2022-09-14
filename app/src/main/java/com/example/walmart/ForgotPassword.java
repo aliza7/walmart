0150032af495936f1db2938baa51949e326e4ec9
aliza7 <84975787+aliza7@users.noreply.github.com>
2022-09-14 09:01:39 +0545

    first commit

---
+++ app/src/main/java/com/example/walmart/ForgotPassword.java
@@ -0,0 +1,65 @@
+package com.example.walmart;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class ForgotPassword extends AppCompatActivity {
+    EditText forgot_email;
+    Button forgot_btn;
+    FirebaseAuth firebaseAuth;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forgot_password);
+
+        forgot_email=findViewById(R.id.et_email);
+        forgot_btn=findViewById(R.id.btn_login);
+        firebaseAuth= FirebaseAuth.getInstance();
+
+        forgot_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Getemail();
+            }
+        });
+    }
+
+    private void Getemail() {
+        String email = forgot_email.getText().toString().trim();
+        if (email.isEmpty()) {
+            forgot_email.setError("Email is require");
+            forgot_email.requestFocus();
+        }
+        firebaseAuth.sendPasswordResetEmail(email).addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                if (task.isSuccessful()) {
+                    Toast.makeText(ForgotPassword.this, "Check Email", Toast.LENGTH_SHORT).show();
+                }else{
+                    Toast.makeText(ForgotPassword.this, "Check Email", Toast.LENGTH_SHORT).show();
+
+                }
+
+            }
+        });
+    }
+
+    public void back_arrow_login(View view) {
+        Intent intent=new Intent(getApplicationContext(),MainActivity.class);
+        startActivity(intent);
+    }
+}
